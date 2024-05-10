c32bbdc6d27f8e659a53004a61cb47acf5f71b1e
Rogério Ferreira Brito <rogerio.ferreira@ifood.com.br>
2024-05-10 00:03:26 -0300

    adding item-api-app

---
+++ k8s-deployments/item-api-app/src/test/java/com/litmusplayground/itemapiapp/ItemApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litmusplayground.itemapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
