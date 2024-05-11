cdfc64358d9e57e37024f0bc4f6ff9ab64fcc65f
Rogério Ferreira Brito <rogerio.ferreira@ifood.com.br>
2024-05-10 23:03:23 -0300

    adding checks to k6 script

--- k8s-deployments/k6-loadtest/script.js
+++ k8s-deployments/k6-loadtest/script.js
@@ -1,27 +1,31 @@
 import http from 'k6/http';
+import { check, fail } from 'k6';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export default function () {
-  let headers = {
+  const headers = {
     headers: { 'Content-Type': 'application/json' },
   };
-  let order = {
+  const order = {
     customerName: randomString(8)
   };
 
-  let orderRes = http.post(`http://${__ENV.ORDER_HOST}:${__ENV.ORDER_PORT}/v1/order`, JSON.stringify(order), headers);
-
-  if (orderRes.status !== 200) {
-    return;
+  const orderRes = http.post(`http://${__ENV.ORDER_HOST}:${__ENV.ORDER_PORT}/v1/order`, JSON.stringify(order), headers);
+  if (!check(orderRes, { 'order create status code MUST be 200': (res) => res.status == 200, })) {
+    fail('order create status code was *not* 200');
   }
 
   for (var i = 0; i < 10; i++) {
-    let item = {
+    const item = {
       itemName: randomString(8),
       quantity: randomIntBetween(1, 50),
       price: randomIntBetween(1, 10000) / 10
     }
-    http.put(`http://${__ENV.ORDER_HOST}:${__ENV.ORDER_PORT}/v1/order/${orderRes.json().orderID}/item`, JSON.stringify(item), headers);
+
+    const itemRes = http.put(`http://${__ENV.ORDER_HOST}:${__ENV.ORDER_PORT}/v1/order/${orderRes.json().orderID}/item`, JSON.stringify(item), headers);
+    if (!check(itemRes, { 'item create status code MUST be 200': (res) => res.status == 200, })) {
+      fail('item create status code was *not* 200');
+    }
   }
 }
