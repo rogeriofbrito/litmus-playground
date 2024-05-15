f8ad733f66a7da3bedf29b4057d5283ca586ac9b
Rogério Ferreira Brito <rogerio.ferreira@ifood.com.br>
2024-05-15 18:20:43 -0300

    k6-probe

--- k8s-deployments/probe-api-app/src/main.go
+++ k8s-deployments/probe-api-app/src/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	e := echo.New()
 
-	e.POST("/probe/k6/smoke-test-order-api", func(c echo.Context) error {
+	e.GET("/probe/k6/smoke-test-order-api", func(c echo.Context) error {
 		// TODO: generate random pod name
 		cmd := exec.Command("/bin/sh", "-c", "kubectl run k6-probe -n probe-api-app --env=PROBE_ID=smoke-test-order-api --env=ORDER_HOST=order-api-app-service.order-api-app.svc.cluster.local --env=ORDER_PORT=8080 --image-pull-policy=IfNotPresent --image=localhost:5000/k6-probe:latest --restart=Never --rm -it")
 
@@ -20,7 +20,7 @@ func main() {
 		return c.NoContent(http.StatusOK)
 	})
 
-	if err := e.Start("127.0.0.1:8080"); err != nil {
+	if err := e.Start(":8080"); err != nil {
 		panic(err)
 	}
 }
